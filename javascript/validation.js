351af6d6c3b75a722d32c0081f277c34d02c79ec
blerinakuka <kukajblerina@gmail.com>
2023-02-19 01:46:10 +0100

    touch up validation

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,12 +1,12 @@
-function validateLogin() {
-  var username = document.forms["loginForm"]["username"].value;
-  var password = document.forms["loginForm"]["password"].value;
+function validated() {
+  var username = document.forms["form"]["username"].value;
+  var password = document.forms["form"]["password"].value;
 
   var name1_error = document.getElementById("name1_error");
   var password_error = document.getElementById("password_error");
 
   var valid = true;
-  var usernameRegex = /^[a-zA-Z0-9_]{5,}$/; // allow letters, numbers, and underscores, at least 5 characters long
+  var usernameRegex = /^[a-zA-Z0-9_]{3,20}$/; // allow letters, numbers, and underscores, at least 5 characters long
   var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/; // at least 8 characters long, must contain at least one digit, one lowercase letter, and one uppercase letter
 
   if (username == "" || !usernameRegex.test(username)) {
