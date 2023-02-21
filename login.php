7b253646e349648477fa24b33e87b85b14eb5220
blerinakuka <kukajblerina@gmail.com>
2023-02-21 16:28:00 +0100

    update ps

--- login.php
+++ login.php
@@ -1,55 +1,3 @@
-<?php
-
-$server = "localhost";
-$username = "root";
-$password = '';
-$database = "ProjektiWeb";
-
-$data=mysqli_connect($server,$username,$password,$database);
-
-if($data===false){
-  die("connection error");
-}
-
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-  $username=$_POST["username"];
-  $password=$_POST["password"];
-
-  $sql="SELECT * FROM users WHERE username='".$username."' AND password='".$password."' ";
-
-  $result=mysqli_query($data,$sql);
-
-  $row = mysqli_fetch_array($result);
-
-
-  if($row["usertype"]=="user"){
-
-    $_SESSION["username"]=$username;
-    header("location:home.php");
-
-  }
-
-  elseif($row["usertype"]=="admin"){
-
-    $_SESSION["username"]=$username;
-    header("location:dashboard.php");
-    
-  }
-
-  // if(isset($_POST["submit"])){
-  //   $myInput=$_POST["username"];
-  //   if(preg_match("/^[A-Za-z][A-Za-z0-9_]{7,29}$/", $myInput)){
-  //     echo "Valid Name";
-  //   }else{
-  //     echo"Enter Valid Name";
-  //   }
-  // }
- 
-
-}
-
-?><!DOCTYPE html>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -67,60 +15,6 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
   <div class="loginbody">
     <div class="card">
       <div class="inner-box" id="card">
-<<<<<<< HEAD
-      <div class="card-font">
-    <h2>LOGIN</h2>
-    <form class="login_form" action="" method="post" name="form" onsubmit="return validated()">
-  <input type="text" class="input-box" name="username" placeholder="Username" required>
-  <div id="name1_error">Fill out your username correctly</div>
-  <input type="password" class="input-box" name="password" placeholder="Password" required>
-  <div id="password_error">Fill out your password correctly</div>
-   <button type="submit" class="submit-btn" >Submit</button>   
-  </form>
-  <a href="register.php" class="acc">Don't have an account?</a>
-      </div>
-     </div>
-     </div>
-     </div>
- <section>
-  <section>
-    <div class="contact">
-      <div class="contact1">
-      <h1>Contact Us</h1><br>
-      <h3 class="nr1">+383 1000 1000 </h3>
-    </div>
-    <div class="hotline">
-      <h1>Hotline</h1><br>
-      <h3 class="nr2">Monday and Thursday: 9am – 1pm</h3>
-    </div>
-    </div>
-    </section>
-<footer class="footer">
-  <div class="container1">
-    <div class="row">
-      <div class="footer-col">
-        <h4>Company</h4>
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="about.html">About Us</a></li>
-          <li><a href="shop.html">Products</a></li>
-        </ul>
-      </div>
-      <div class="footer-col">
-        <h4>Highlights</h4>
-        <ul>
-          <li><a href="shop.html">Cleanser</a></li>
-          <li><a href="shop.html">Rose Water  <br>Toner</a></li>
-          <li><a href="shop.html">Brightening <br> Moisturizer</a></li>
-        </ul>
-      </div>
-      <div class="footer-col">
-        <h4>follow us</h4>
-          <div class="social-links">
-            <a href="#"><i class="fab fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-instagram"></i></a>
-
         <div class="card-font">
           <h2>LOGIN</h2>
           <form class="login_form" action="./login_handler.php" method="post" name="form" onsubmit="return validated()">
@@ -131,7 +25,6 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
             <button type="submit" class="submit-btn">Submit</button>   
           </form>
           <a href="register.php" class="acc">Don't have an account?</a>
-
         </div>
       </div>
     </div>
@@ -141,4 +34,4 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
   ?>
   <script src="javascript/validation.js"></script>
 </body>
-</html>
+</html> 
--- shop.php
+++ shop.php
@@ -1,25 +1,20 @@
-
-
+<?php
+require_once './controllers/MenuControllerr.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Shop</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" type="text/css"  href="css/shop.css">
-    <script src="javascript/shop.js"></script>
-    </head>
+    <script src="javascript/script.js"></script>
+</head>
 <?php
  include 'subComponents/header.php';
 
 ?>
-<?php
-include 'Model.php';
-$model = new Model();
-$rows = $model->shopfetch();
-?>
-
 <body>
   <section class="thenjaa">
     <div class="thenja"> 
@@ -29,79 +24,21 @@ $rows = $model->shopfetch();
     <div class="thenja2">
         <p>Rejuvenate your skin with good-for-you skincare products.</p>
     </div>
-   
-
-    </section>
-<div class="shopi">
-    <div class="produktet">
-        <div class="product ">
-          <img src="<?php echo $rows[0]['image'];?>">
-          <p><a href ="#"><?php echo $rows[0]['name'];?> <br> <?php echo $rows[0]['price'];?></a></p><br>
-          <button type="button">SHOP NOW</button>
-        </div>
-       <div class="product ">
-           <img src="<?php echo $rows[1]['image'];?>">
-           <p><a href ="#"><?php echo $rows[1]['name'];?> <br> <?php echo $rows[1]['price'];?></a></p><br>
-           <button type="button">SHOP NOW</button>
-       </div>
-       <div class="product ">
-         <img src="images/seti3.webp">
-         <p><a href ="#">Serum + moisturizer <br> $80</a></p><br>
-         <button type="button">SHOP NOW</button>
-         </div>
-         <div class="product ">
-            <img src="images/item1.webp">
-            <p><a href ="#">Facial Cleanser <br> $20</a></p><br>
-            <button type="button">SHOP NOW</button>
-          </div>
-       </div>
-  <div class="produktet">
-
-   <div class="product ">
-       <img src="images/item2.webp">
-       <p><a href ="#">Rose Water Toner <br>$17</a></p><br>
-       <button type="button">SHOP NOW</button>
-   </div>
-<div class="product ">
-     <img src="images/item3.webp">
-     <p><a href ="#">Vitamin C Serum<br> $90</a></p><br>
-     <button type="button">SHOP NOW</button>
-   </div>
-   <div class="product ">
-    <img src="images/item4.webp">
-    <p><a href ="#">Retinol Serum <br> $130</a></p><br>
-    <button type="button">SHOP NOW</button>
-  </div>
- <div class="product ">
-     <img src="images/item5.webp">
-     <p><a href ="#">Anti-Aging Eye Cream <br>$112</a></p><br>
-     <button type="button">SHOP NOW</button>
- </div>
-</div>
-<div class="produktet">
-
-   <div class="product ">
-     <img src="images/item6.webp">
-     <p><a href ="#">Hydrating Moisturizer <br> $53</a></p><br>
-     <button type="button">SHOP NOW</button>
-   </div>
-   <div class="product ">
-    <img src="images/item7.webp">
-    <p><a href ="#">Brightening Moisturizer <br> $46</a></p><br>
-    <button type="button">SHOP NOW</button>
-  </div>
- <div class="product ">
-     <img src="images/item8.webp">
-     <p><a href ="#">Retinol Moisturizer <br>$130</a></p><br>
-     <button type="button">SHOP NOW</button>
- </div>
- <div class="product ">
-   <img src="images/item9.webp">
-   <p><a href ="#">Detox  Mask <br> $30</a></p><br>
-   <button type="button">SHOP NOW</button>
-   </div>
-   </div>
-
+</section>
+<div class="krejtt">
+  <?php
+  $productss = new MenuControllerr;
+  $alll = $productss->readData();
+  for($i = 0; $i < count($alll); $i++) {
+    echo '<div class="produktett">
+      <div class="productt">
+        <img src="' .$alll[$i]['menu_image'] .'">
+        <h5>' .$alll[$i]['menu_title'] .'<h5>
+        <p>' .$alll[$i]['menu_body'] .'</p>
+      </div>
+    </div>';
+  } 
+  ?>
 </div>
 
 <?php
